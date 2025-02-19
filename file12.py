6da7fd5a1a3ac38d9c10643d313a93a5545a2c70
Maren Sajdaras <74674192+marensajdaras@users.noreply.github.com>
2025-02-19 13:59:13 +0100

    Add files via upload
    update of file12 but with an chain of interactions and with added memory

--- file12.py
+++ file12.py
@@ -1,14 +1,27 @@
 import os
+import google.generativeai as genai
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+
 
 api_key = os.getenv("GEMINI_API_KEY")
-
-import google.generativeai as genai
 
 # configuring the API key globally
 genai.configure(api_key = api_key)
 
+# variable for the model
 model = genai.GenerativeModel("gemini-pro")
 
+# initialize the memory
+memory = ConversationBufferMemory() 
+
+
+# using langchain class for creating a chain of interactions
+conversation = ConversationChain(
+    llm = model,
+    verbose = True, # during the testing phase we need additionsal info 
+    memory = memory 
+)
 # adding a loop to make a conversation with many prompts
 while True:
 
