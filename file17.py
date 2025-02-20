8b16178e789fc05ab2621f482bd46f44a3afb7c9
Maren Sajdaras <74674192+marensajdaras@users.noreply.github.com>
2025-02-20 21:28:14 +0100

    Add files via upload

--- file17.py
+++ file17.py
@@ -12,19 +12,60 @@ import os
 
 api_key = os.getenv("GEMINI_API_KEY")
 
-chat = ChatGoogleGenerativeAI(model = "gemini-pro", google_api_key = api_key)
+chat = ChatGoogleGenerativeAI(model = "gemini-1.5-pro", google_api_key = api_key) # "gemini-pro"
 
-user_input = input("Human (type 'exit' to end conversation): ")
+# user_input = input("Human (type 'exit' to end conversation): ")
 
-messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "I'm going to Rome for 2 days, what can I visit?"}
-]
+
 
 # Instead of __call__, use invoke to generate the response
-response = chat.invoke(user_input)
+# response = chat.invoke(user_input)
 
-print(response.content)
+# print(response.content)
+
+
+messages = [
+
+    ("system", "You are a helpful assistant that translates English to French. Translate the user sentence."),
+    ("human", "I love programming."),
+
+
+    ]
+
+# ai_msg = chat.invoke(messages)
+
+# print(ai_msg.content)
+
+from langchain.memory import ConversationBufferMemory
+
+memory = ConversationBufferMemory()
+
+conversation = ConversationChain(
+    llm = chat,
+    verbose = True,
+    memory = memory,
+)
+
+# a = conversation.run("Hi there Gemini how are you?")
+
+# print(a)
+
+
+while True:
+
+    query = input('you:  ')
+
+    if query == 'exit':
+        break
+
+    output = conversation({'input': query})
+
+    print('AI Agent: ', output['response'])
+
+
+    
+
+    
 
 
 
